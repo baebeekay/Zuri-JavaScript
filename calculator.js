f34caa6d8d3e480743b9511d03e475c80108601b
baebeekay <baebeekay6@gmail.com>
2022-06-20 22:00:57 +0100

    File error fix

--- calculator.js
+++ calculator.js
@@ -1,28 +1,25 @@
 // adds two numbers
 function add(num1, num2) {
-    return num1 + num2;
+    console.log(num1 + num2);
     
 }
 
 // multiplies two numbers
 function mul(num1, num2) {
-    return num1 * num2;
+    console.log(num1 * num2);
     
 }
 
 // subtracts two numbers
 function sub(num1, num2) {
-    return num1 - num2;
+    console.log(num1 - num2);
     
 }
 
 // divides two numbers
 function div(num1, num2) {
-    return num1 / num2;
+    console.log(num1 / num2);
     
 }
 
-console.log(add(1, 1))
-console.log(mul(2, 2))
-console.log(sub(4, 2))
-console.log(div(4, 2))
+
---
+++ print.js
@@ -0,0 +1,4 @@
+console.log("My Name is Amarachi Kalu Ndukwe");
+console.log("Height: 169");
+console.log("Country: Nigeria");
+
