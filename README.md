fe43de0efe88eac3548ec405625e2d4e20875e9b
baebeekay <baebeekay6@gmail.com>
2022-06-20 21:49:29 +0100

    Task files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Zuri-JavaScript
-Zuri Tasks
+
+print.js  -  a javascript code that prints out my name, height, and country on the screen.
+
+calculator.js - performs basic operations like Addition, Subtraction, Multiplication, & Division.
---
+++ calculator.js
@@ -0,0 +1,28 @@
+// adds two numbers
+function add(num1, num2) {
+    return num1 + num2;
+    
+}
+
+// multiplies two numbers
+function mul(num1, num2) {
+    return num1 * num2;
+    
+}
+
+// subtracts two numbers
+function sub(num1, num2) {
+    return num1 - num2;
+    
+}
+
+// divides two numbers
+function div(num1, num2) {
+    return num1 / num2;
+    
+}
+
+console.log(add(1, 1))
+console.log(mul(2, 2))
+console.log(sub(4, 2))
+console.log(div(4, 2))
